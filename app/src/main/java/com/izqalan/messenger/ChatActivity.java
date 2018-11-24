f0c14fe5116c466d1e7edd42645e8911ed076b96
izqalan <izqalan@gmail.com>
2018-11-25 04:17:25 +0800

    added chat bubble

--- app/src/main/java/com/izqalan/messenger/ChatActivity.java
+++ app/src/main/java/com/izqalan/messenger/ChatActivity.java
@@ -6,16 +6,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +30,9 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -44,10 +50,21 @@ public class ChatActivity extends AppCompatActivity {
 
     private String chatUser;
     private String username;
+    private String username2;
+    private String currentUID;
 
     private DatabaseReference usersDatabase;
     private DatabaseReference rootRef;
+    private DatabaseReference messageDatabase;
     private FirebaseAuth mAuth;
+
+    // message list here
+    private RecyclerView messagesList;
+    private final List<Messages> msgList = new ArrayList<>();
+    private LinearLayoutManager linearLayout;
+    private MessagesAdapter messagesAdapter;
+    private TextView messageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,13 +72,15 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         // firebase instantiation
-        usersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-        rootRef = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+        usersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
 
         // get value from previous intent
         chatUser = getIntent().getStringExtra("user_id");
         username = getIntent().getStringExtra("name");
+        currentUID = mAuth.getCurrentUser().getUid();
         final String thumb_image = getIntent().getStringExtra("avatar");
 
         // Action bar & toolbar
@@ -87,6 +106,21 @@ public class ChatActivity extends AppCompatActivity {
         sendBtn = findViewById(R.id.send_btn);
 
 
+        messageText = findViewById(R.id.text_content);
+
+        // chat layout
+        messagesAdapter = new MessagesAdapter(msgList);
+
+        messagesList = findViewById(R.id.messages_list);
+        linearLayout = new LinearLayoutManager(this);
+
+        messagesList.setHasFixedSize(true);
+        messagesList.setLayoutManager(linearLayout);
+        messagesList.setAdapter(messagesAdapter);
+
+        loadMessages();
+
+
         nameView.setText(username);
 
         Picasso.get().load(thumb_image).placeholder(R.drawable.default_avatar).networkPolicy(NetworkPolicy.OFFLINE).into(profileImage, new Callback() {
@@ -104,6 +138,18 @@ public class ChatActivity extends AppCompatActivity {
                         .placeholder(R.drawable.default_avatar)
                         .error(R.drawable.default_avatar)
                         .into(profileImage);
+
+            }
+        });
+
+        usersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                username2 = dataSnapshot.child("name").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
@@ -152,6 +198,46 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
+    private void loadMessages() {
+
+
+        rootRef.child("Messages").child(mAuth.getCurrentUser().getUid()).child(chatUser).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                Messages message = dataSnapshot.getValue(Messages.class);
+
+                String x = message.getMessage().toString();
+                Log.e("MESSAGE_GET: ", x);
+
+                msgList.add(message);
+                messagesAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     private void sendMessage() {
 
         String msg = messageField.getText().toString();
@@ -168,12 +254,19 @@ public class ChatActivity extends AppCompatActivity {
 
             String pushId = pushMessage.getKey();
 
+
+            Log.e("name is", username2);
+
+
             // values of the /Message/currID/uid/msgID
             Map msgValMap = new HashMap();
             msgValMap.put("message", msg);
+            msgValMap.put("from", currentUID);
+            msgValMap.put("name", username2);
             msgValMap.put("seen", false);
             msgValMap.put("type", "text");
             msgValMap.put("time", ServerValue.TIMESTAMP);
+
 
             // store msg Id into table for each users
             Map storeMsgMap = new HashMap();
---
+++ app/src/main/java/com/izqalan/messenger/MessagesAdapter.java
@@ -0,0 +1,100 @@
+package com.izqalan.messenger;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessagesViewHolder>{
+
+    private List<Messages> mMessagesList;
+    private DatabaseReference userDatabase;
+    private FirebaseAuth mAuth;
+
+    public MessagesAdapter(List<Messages> messageIn) {
+
+        mAuth = FirebaseAuth.getInstance();
+        this.mMessagesList = messageIn;
+
+    }
+
+    @Override
+    public MessagesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.message_layout, parent, false);
+        return new MessagesViewHolder(v);
+
+    }
+
+    public class MessagesViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView timeText;
+        public TextView messageText;
+        public TextView displayNameText;
+
+        public MessagesViewHolder(View view){
+            super (view);
+            messageText = view.findViewById(R.id.text_content);
+            displayNameText = view.findViewById(R.id.display_name);
+
+        }
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(final MessagesViewHolder viewHolder, int i) {
+
+        String currentUID = mAuth.getCurrentUser().getUid();
+
+        // all the messages stored inside messagelist<array list>
+        // get messages by using index
+        Messages c = mMessagesList.get(i);
+
+        String fromUser = c.getFrom();
+        Log.e("From User", fromUser);
+
+        // do not use currentUID. .equals() can only pass Objects
+        if(fromUser.equals(mAuth.getCurrentUser().getUid())){
+
+            // change the layout
+            viewHolder.messageText.setBackgroundColor(Color.WHITE);
+            viewHolder.messageText.setTextColor(Color.BLACK);
+
+        }else {
+
+            // change layout
+            viewHolder.messageText.setBackgroundResource(R.drawable.message_bubble);
+            viewHolder.messageText.setTextColor(Color.WHITE);
+
+        }
+
+        String z = c.getMessage();
+        Log.e("FUCK", z);
+        //
+        viewHolder.messageText.setText(c.getMessage());
+        viewHolder.displayNameText.setText(c.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessagesList.size();
+    }
+
+}
