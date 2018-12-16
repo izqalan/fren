2c88988ca6c5cbc7a216c038d3537a4b50583827
izqalan <izqalan@gmail.com>
2018-12-17 00:08:47 +0800

    added places autocomplete adapter

--- app/src/main/java/com/izqalan/messenger/MapsActivity.java
+++ app/src/main/java/com/izqalan/messenger/MapsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -12,29 +13,40 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private PlaceAutocompleteAdapter placeAutocompleteAdapter;
 
-    private EditText searchText;
+    private AutoCompleteTextView searchText;
     private ImageView gpsImg;
 
     final String TAG = "Maps Activity";
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136)
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +64,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         init();
 
     }
+    // required by google places API
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
     private void init(){
+
+        placeAutocompleteAdapter = new PlaceAutocompleteAdapter(this,
+                Places.getGeoDataClient(this), LAT_LNG_BOUNDS, null);
+
+        // set adapter to auto complete textview
+
+        searchText.setAdapter(placeAutocompleteAdapter);
+
         searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -159,4 +183,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        init();
 
     }
+
+
 }
